0e990ba0ab37ed25735ece241762d7c913369647
wjy <xue2468008>
2018-01-02 11:06:40 +0800

    照片墙文件夹路径调整

---
+++ 照片墙/照片墙2.0/data.js
@@ -0,0 +1,35 @@
+var data =[
+	{
+		img : '1.jpg',
+		name : 'cat1'
+	},
+		{
+		img : '2.jpg',
+		name : 'cat2'
+	},
+		{
+		img : '3.jpg',
+		name : 'cat3'
+	},
+		{
+		img : '4.jpg',
+		name : 'cat4'
+	},
+		{
+		img : '5.jpg',
+		name : 'cat5'
+	},
+		{
+		img : '6.jpg',
+		name : 'cat6'
+	},
+		{
+		img : '7.jpg',
+		name : 'cat7'
+	},
+	{
+		img : '8.jpg',
+		name : 'cat8'
+	}
+
+];
