048653ae68a2f232dee8994b86e17dd88bab3bf5
weijiyang <845612246@qq.com>
2018-01-09 10:54:46 +0800

    更改结构

--- 毕业设计/高校兼职招聘系统/Application/Home/Controller/IndexController.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Home\Controller;
-use Think\Controller;
-class IndexController extends Controller {
-    public function index(){
-    	echo "这里是index controller ！";
-        $this->show();
-    }
-}
--- 毕业设计/高校兼职招聘系统/Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
+++
@@ -1,9 +0,0 @@
-<?php if (!defined('THINK_PATH')) exit();?><html>
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-</head>
-<body>
-	这里是 INDEX 文件夹下面的   index.html文件
-</body>
-</html>
