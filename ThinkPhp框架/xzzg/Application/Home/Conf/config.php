1f16ff58d3996f1428bfe5f75e831d5ff7d4316b
weijiyang <845612246@qq.com>
2018-01-11 10:51:02 +0800

    删除thinkph结构

--- ThinkPhp框架/xzzg/Application/Home/Conf/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-	//'配置项'=>'配置值'
-	'DB_TYPE'               =>  'mysql',     // 数据库类型
-    'DB_HOST'               =>  'localhost', // 服务器地址
-    'DB_NAME'               =>  'xzzg',          // 数据库名
-    'DB_USER'               =>  'root',      // 用户名
-    'DB_PWD'                =>  '123456'         // 密码
-);
