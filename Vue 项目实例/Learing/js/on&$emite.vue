e3a28a773d7fc714b66ce50bf4d0e5a66eb38c14
weijiyang <845612246@qq.com>
2018-02-02 15:54:07 +0800

    on &

--- Vue 项目实例/Learing/js/on&$emite.vue
+++ Vue 项目实例/Learing/js/on&$emite.vue
@@ -1,1 +1,16 @@
+<my-component
+ 	v-for="(item,index) in items"
+ 	v-bind:item = "item"
+ 	v-bind:index = "index"
+ 	v-bind:key = "item.id">
+</my_component>
 
+Vue.component('todo-item', {
+  template: '\
+    <li>\
+      {{ title }}\
+      <button v-on:click="$emit(\'remove\')">X</button>\
+    </li>\
+  ',
+  props: ['title']
+})
